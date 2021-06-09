5ea89189bb1773aa7e37114cd584ad42ef9c9ad0
Hiren Gondhiya <hirengondhiya@gmail.com>
2021-06-09 11:20:11 +1000

    button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+const Button = ({ children, loading, ...props }) => (
+  <button className="button" disabled={loading} {...props}>
+    {loading ? "Loading..." : children}
+  </button>
+);
+
+Button.defaultProps = {
+  loading: false,
+};
+
+export default Button;
